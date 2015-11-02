3b66f3863d124b40604c074458189d0a1ae6eb6e
Donald Sears <kasp3rd@gmail.com>
2015-11-01 21:30:35 -0700

    working...

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2011 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 
-PRODUCT_COPY_FILES += \
-    device/lge/h811/kernel:kernel
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_h901.mk
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,21 +1,3 @@
-#
-# Copyright (C) 2015 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-TARGET_OTA_ASSERT_DEVICE := v10,h901
-
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8992
 TARGET_NO_BOOTLOADER := true
@@ -30,42 +12,44 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
+TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := generic
 
-# Kernel
-TARGET_PREBUILT_KERNEL := device/lge/h901/kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.hardware=pplus
+ 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02200000 --dt device/lge/h901/dt.img
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x0000000 --ramdisk_offset 0x02200000 --tags_offset 0x00000100 --dt device/lge/h901/dt.img
 
-# Init
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+# prebuilt kernel
+TARGET_PREBUILT_KERNEL := device/lge/h901/kernel
+# else uncomment below to build from sauce
+# TARGET_KERNEL_SOURCE := kernel/lge/h901
+# TARGET_KERNEL_CONFIG := pplus_tmo_us-perf_defconfig
 
-# Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 41943040
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 41943040
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4492099584
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 24935137280
-BOARD_FLASH_BLOCK_SIZE := 262144
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
-# Recovery
-TARGET_RECOVERY_DEVICE_DIRS += device/lge/h811
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_888"
+#TARGET_RECOVERY_FSTAB := device/lge/h901/twrp.fstab
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
-# TWRP
 TW_THEME := portrait_hdpi
-BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
-RECOVERY_SDCARD_ON_DATA := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_INCLUDE_CRYPTO := true
 TW_NO_USB_STORAGE := true
+TW_INCLUDE_CRYPTO := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
 TW_SCREEN_BLANK_ON_BOOT := true
